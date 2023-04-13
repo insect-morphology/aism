513acc53fd69e5a6bbfa69e7f78bfb37f810b441
sergeitarasov <sergxf@yandex.ru>
2023-04-13 23:43:59 +0300

    'cuticle of' changed

--- src/py-scripts/change_labels.py
+++ src/py-scripts/change_labels.py
@@ -39,3 +39,48 @@ onto.save(file = saveFile, format = "rdfxml")
 # Replace manually
 # '"en">cuticle ' of TO '"en">'
 # '--cuticle of' TO 'cuticle of'
+
+
+
+
+# -------------
+# COLAO
+# -------------
+
+ontoFile= '/Users/taravser/Documents/My_papers/CoLAO/colao/src/ontology/colao-edit.owl'
+saveFile= '/Users/taravser/Documents/My_papers/CoLAO/colao/src/ontology/colao-edit-2.owl'
+onto = get_ontology(ontoFile).load(reload_if_newer=True, reload=True)
+obo = onto.get_namespace("http://purl.obolibrary.org/obo/")
+
+# 'OBO foundry unique label’
+# - http://purl.obolibrary.org/obo/IAO_0000589
+# obo.IAO_0000589.python_name
+
+found_terms = onto.search(label='cuticle of*', iri = '*COLAO*')
+len(found_terms)
+
+filename = '/Users/taravser/Documents/My_papers/AISM/GitHub/aism/src/py-scripts/found_terms_colao.txt'
+i=1
+with open(filename, 'w') as f:
+    for term in found_terms:
+        old_label = term.label.first()
+        print(i, old_label,  file=f)
+        i=i+1
+    f.close()
+
+
+i=1
+for term in found_terms:
+    old_label = term.label.first()
+    # new_label = old_label.replace('cuticle of ', '')
+    print(i, old_label)
+    i=i+1
+    # term.label = new_label
+    obo.IAO_0000589[term] = '--' + old_label
+
+onto.save(file = saveFile, format = "rdfxml")
+
+
+# Replace manually
+# '"en">cuticle of ' of TO '"en">'
+# '--cuticle of' TO 'cuticle of'
